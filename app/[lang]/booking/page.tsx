dad8cab11a82b6a523e87d54efbaab57514d3db8
Yana Burdakova <bu.asti@gmail.com>
2023-06-28 16:10:53 -0400

    trying#2 to fix booking widget

--- app/[lang]/booking/page.tsx
+++ app/[lang]/booking/page.tsx
@@ -8,10 +8,9 @@ function BookingPage() {
         <div>Страница бронирования</div>
         <Script id="shelter-script" src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js"></Script>
         <div id="book-widget"></div>
-        <Script id="shelter-widget" strategy="lazyOnload">
-            {`
-            FD24BookWidget.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);
-            `}
+        <Script 
+            id="shelter-widget" 
+            src={`FD24BookWidget.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);`}>
         </Script>
         </div>
     );
