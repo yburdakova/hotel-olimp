c63b25f7e001beca648390d8b1d30800b4588dec
Yana Burdakova <bu.asti@gmail.com>
2023-06-28 16:07:28 -0400

    tring to fix error with booking widget

--- app/[lang]/booking/page.tsx
+++ app/[lang]/booking/page.tsx
@@ -2,20 +2,19 @@ import React from 'react';
 import Script from 'next/script';
 
 
-
 function BookingPage() {
-
-
     return (
         <div>
-            <div className="">Booking Page</div>
-            <Script id="shelter-script" src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js"></Script>
-                <div id="book-widget"></div>
-                <Script id="shelter-widget">
-                    FD24BookWidget.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);
-                </Script>
+        <div>Страница бронирования</div>
+        <Script id="shelter-script" src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js"></Script>
+        <div id="book-widget"></div>
+        <Script id="shelter-widget" strategy="lazyOnload">
+            {`
+            FD24BookWidget.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);
+            `}
+        </Script>
         </div>
-    )
+    );
 }
 
-export default BookingPage
+export default BookingPage;
