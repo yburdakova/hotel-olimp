2c4aebeb4f8aac08aea7b192920a077301aa708d
Yana Burdakova <bu.asti@gmail.com>
2023-06-28 16:23:53 -0400

    widget

--- app/[lang]/booking/page.tsx
+++ app/[lang]/booking/page.tsx
@@ -4,15 +4,15 @@ import Script from 'next/script';
 function BookingPage() {
     return (
         <div>
-        <div>Страница бронирования</div>
-        <Script id="shelter-script" src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js"></Script>
-        <div id="book-widget"></div>
-        <Script
-            id="shelter-widget"
-            dangerouslySetInnerHTML={{
-            __html: `FD24BookWidget.createWidget(&quot;book-widget&quot;, &quot;44B8E6C8-3825-46E8-B693-8C551F0780E9&quot;, undefined);`,
-            }}
-        ></Script>
+            <div>Страница бронирования</div>
+            <Script id="shelter-script" src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js" strategy="afterInteractive"></Script>
+            <div id="book-widget"></div>
+            <Script
+                id="shelter-widget"
+                dangerouslySetInnerHTML={{
+                    __html: `FD24BookWidget.createWidget(&quot;book-widget&quot;, &quot;44B8E6C8-3825-46E8-B693-8C551F0780E9&quot;, undefined);`,
+                }}
+            />
         </div>
     );
 }
