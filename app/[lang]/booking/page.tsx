732ecdf0d4f6946b84e28fb9c15b44368d79e731
Yana Burdakova <bu.asti@gmail.com>
2023-06-28 17:08:09 -0400

    fixing

--- app/[lang]/booking/page.tsx
+++ app/[lang]/booking/page.tsx
@@ -7,13 +7,14 @@ const BookingPage = () => {
     return (
         <div>
         <Helmet>
-            <Script src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js" />
+            <Script id="book-widget-script" src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js" />
             </Helmet>
             <Script
-            dangerouslySetInnerHTML={{
-                __html: 'FD24BookWidget.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);',
-            }}
-            />
+                id="book-widget-2"
+                dangerouslySetInnerHTML={{
+                    __html: 'FD24BookWidget.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);',
+                }}
+                />
             <div id="book-widget" />
             <div>BOOKING PAGE</div>
         </div>
