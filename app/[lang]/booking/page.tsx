f23e3fdf7d6f569850ed103014be8019f84700fd
Yana Burdakova <bu.asti@gmail.com>
2023-06-28 17:41:13 -0400

    fixing widget 17:40

--- app/[lang]/booking/page.tsx
+++ app/[lang]/booking/page.tsx
@@ -7,11 +7,6 @@ function BookingPage() {
         <div>
             <div className="">BOOKING PAGE</div>
 
-        <Script id="booking-widget-code" src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js"></Script>
-            <div id="book-widget"></div>
-        <Script id="booking-widget">
-            FD24BookWidget.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);
-        </Script>
         </div>  
     )
 }
