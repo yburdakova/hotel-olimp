a54315e2381c095fcd268e71dc6912ef46fa9e5d
Yana Burdakova <bu.asti@gmail.com>
2023-06-28 15:51:44 -0400

    Shelter widget is added

--- app/[lang]/booking/page.tsx
+++ app/[lang]/booking/page.tsx
@@ -18,9 +18,9 @@ function BookingPage() {
     return (
         <div>
             <div className="">Booking Page</div>
-            <Script id="shelter" src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js"></Script>
+            <Script id="shelter-script" src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js"></Script>
                 <div id="book-widget"></div>
-                <Script>
+                <Script id="shelter-widget">
                     code.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);
                 </Script>
         </div>
