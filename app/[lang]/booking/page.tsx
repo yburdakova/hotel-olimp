4636a9ebcd67b37f334c91bda359c0fb3896553d
Yana Burdakova <bu.asti@gmail.com>
2023-06-28 16:48:46 -0400

    fixing widget 16:50

--- app/[lang]/booking/page.tsx
+++ app/[lang]/booking/page.tsx
@@ -2,31 +2,18 @@
 import React, { useEffect } from 'react';
 import Script from 'next/script';
 
-const BookingPage = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://pms.frontdesk24.ru/onlineWidget/bookWidget.js';
-    script.async = true;
-    document.body.appendChild(script);
+const BookingPage = ({ Component, pageProps }:any) => {
+    return (
+        <>
+          <Component {...pageProps} />
+          <Script src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js" id="booking"></Script>
+                <div id="book-widget"></div>
+                <Script id="shelter">
+                    {`FD24BookWidget.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);`}
+                </Script>
+        </>
+      )
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (window.FD24BookWidget) {
-      window.FD24BookWidget.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);
-    }
-  }, []);
-
-  return (
-    <div>
-      <div>BOOKING PAGE</div>
-      <div id="book-widget" />
-      <Script id="book-widget-script">{`FD24BookWidget.createWidget(&quot;book-widget&quot;, &quot;44B8E6C8-3825-46E8-B693-8C551F0780E9&quot;, undefined);`}</Script>
-    </div>
-  );
 };
 
 export default BookingPage;
