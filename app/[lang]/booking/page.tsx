ccbcc19021a9d5ae0f7dda66fcd4cf41140298dc
Yana Burdakova <bu.asti@gmail.com>
2023-06-28 17:11:54 -0400

    fixed widget 17:20

--- app/[lang]/booking/page.tsx
+++ app/[lang]/booking/page.tsx
@@ -12,7 +12,7 @@ const BookingPage = () => {
             <Script
                 id="book-widget-2"
                 dangerouslySetInnerHTML={{
-                    __html: 'FD24BookWidget.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);',
+                    __html: 'FD24BookWidget.createWidget(&quot;book-widget&quot;, &quot;44B8E6C8-3825-46E8-B693-8C551F0780E9&quot;, undefined);',
                 }}
                 />
             <div id="book-widget" />
