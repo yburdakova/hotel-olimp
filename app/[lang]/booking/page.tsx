071520985e24dfbc9a97adbb0b50f3e2583c4260
Yana Burdakova <bu.asti@gmail.com>
2023-06-28 17:04:39 -0400

    fixing to widget

--- app/[lang]/booking/page.tsx
+++ app/[lang]/booking/page.tsx
@@ -1,19 +1,23 @@
 'use client'
-import React, { useEffect } from 'react';
+import React from 'react';
+import {Helmet} from 'react-helmet'
 import Script from 'next/script';
 
-const BookingPage = ({ Component, pageProps }:any) => {
+const BookingPage = () => {
     return (
-        <>
-          <Component {...pageProps} />
-          <Script src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js" id="booking"></Script>
-                <div id="book-widget"></div>
-                <Script id="shelter">
-                    {`FD24BookWidget.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);`}
-                </Script>
-        </>
-      )
-
+        <div>
+        <Helmet>
+            <Script src="https://pms.frontdesk24.ru/onlineWidget/bookWidget.js" />
+            </Helmet>
+            <Script
+            dangerouslySetInnerHTML={{
+                __html: 'FD24BookWidget.createWidget("book-widget", "44B8E6C8-3825-46E8-B693-8C551F0780E9", undefined);',
+            }}
+            />
+            <div id="book-widget" />
+            <div>BOOKING PAGE</div>
+        </div>
+    );
 };
 
 export default BookingPage;
